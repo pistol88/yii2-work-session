8e672c63f16138ac601fbe51c8da53fda542ceb9
pistol88 <pistol88@gmail.com>
2016-07-05 17:24:29 +0500

    fix date format

--- Session.php
+++ Session.php
@@ -77,51 +77,37 @@ class Session extends Component
     
     private static function getDate($date)
     {
-        $days = floor($date/86400);
         $hours = floor($date/(60*60));
-        $min = floor($date/60);
-
-        if($days > 0) {
-            switch(substr($days, -2)) {
-                case 1: $d = 'день';
-                break;
-                case 2: case 3: case 4: $d = 'дня';
-                break;
-                default: $d = 'дней';
-            }
+        
+        if($hours > 0) {
+            $min = floor(($date-($hours*(60*60)))/60);
+        } else {
+            $min = floor($date/60);
         }
 
+        $return = '';
+        
         if($hours > 0) {
-            switch(substr($hours, -2)) {
+            switch(substr($hours, -1)) {
                 case 1: $h = 'час';
                 break;
                 case 2: case 3: case 4: $h = 'часа';
                 break;
                 default: $h = 'часов';
             }
+            
+            $return .= ' '.$hours.' '.$h;
         }
 
         if($min > 0) {
-            switch(substr($min, -2)) {
+            switch(substr($min, -1)) {
                 case 1: $m = 'минута';
                 break;
                 case 2: case 3: case 4: $m = 'минуты';
                 break;
                 default: $m = 'минут';
             }
-        }
-
-        $return = '';
-        
-        if ($days > 0) {
-            $return .= $days.' '.$d;
-        }
-
-        if ($hours > 0) {
-            $return .= ' '.$hours.' '.$h;
-        }
-
-        if ($min > 0) {
+            
             $return .= ' '.$min.'&nbsp;'.$m;
         }
 
