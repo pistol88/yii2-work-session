dded22c3e7570a33792b0879db8ab226d5082c4c
Ilya <php@php.name>
2016-09-22 18:12:11 +0500

    return web

---
+++ web/js/scripts.js
@@ -0,0 +1,64 @@
+if (typeof pistol88 == "undefined" || !pistol88) {
+    var pistol88 = {};
+}
+
+pistol88.worksess = {
+    init: function() {
+        $(document).on('mouseenter','.worksess-table td', this.renderCross);
+        $(document).on('click', '.worksess-button', this.control)
+    },
+    control: function() {
+        if($(this).hasClass('worksess-start')) {
+            pistol88.worksess.start($(this).attr('href'), this);
+        } else if($(this).hasClass('worksess-stop')) {
+            pistol88.worksess.stop($(this).attr('href'), this);
+        }
+
+        return false;
+    },
+    start: function(url, link) {
+        pistol88.worksess.sendData(url, link);
+        
+        $(document).trigger("workSessionStart", this);
+    },
+    stop: function(url, link) {
+        if(confirm('Действительно?')) {
+            pistol88.worksess.sendData(url, link);
+            
+            $(document).trigger("workSessionStop", this);
+        }
+    },
+    sendData: function(url, link) {
+        $('.worsess-error').remove();
+        $.post(url, {ajax: true},
+            function(json) {
+                if(json.result == 'fail') {
+                    $(link).after('<p class="worsess-error" style="color: red;">'+json.error+'</p>');
+                    console.log(json.error);
+                }
+                else {
+                    var userId = $(link).data('user-id');
+
+                    if(userId) {
+                        $('.worksess-info'+userId).replaceWith(json.info);
+                    } else {
+                        $('.worksess-info').replaceWith(json.info);
+                        document.location.reload();
+                    }
+                    
+                    $(link).replaceWith(json.button);
+                }
+            }, "json");
+    },
+    renderCross: function () {
+        var table = $(this).parents('.worksess-table');
+        $('.worksess-table td').removeClass('hover');
+        var tr = $(this).parent('tr');
+        var Col = tr.find('td').index(this);
+
+        tr.find('td').addClass('hover');
+        $(table).find('tr').find('td:eq(' + Col + ')').addClass('hover');
+    },
+};
+
+pistol88.worksess.init();
